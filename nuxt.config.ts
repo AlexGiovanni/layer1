5ce25c96bc75ef779029404e966d5b593abf3597
Alexis Santiago <alexis.santiago@taxact.com>
2023-11-28 22:33:04 -0600

    extend from nuxtLayers repository

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  extends: [         // Extend from an installed npm package
+    'github:AlexGiovanni/nuxtLayers', // Extend from a git repository
+  ]
 })
